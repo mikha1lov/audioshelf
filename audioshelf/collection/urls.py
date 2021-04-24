0a5a6b796f03e3f4cd51f7cc312d1e955655abde
Vladimir Shemankov <vladimir.shemankov@gmail.com>
2021-04-24 16:10:56 +0300

    [project] rearrange directories around

--- audioshelf/collection/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('tracks', views.TracksList.as_view(), name='tracks_list'),
-    path('tracks/new', views.TrackView.as_view(), name='create_track'),
-]
