b31509240fe40c5d6a676f92f94d0d672f5d83de
tosh <tosh.the@gmail.com>
2021-04-24 16:51:30 +0300

    tests pt1

---
+++ tests/_test_urls.py
@@ -0,0 +1,24 @@
+from http import HTTPStatus
+
+from django.test import TestCase, Client
+from django.urls import reverse
+
+class URLsTest (TestCase):
+    def setUp (self):  # !
+        self.guest_client = Client()
+
+    def test_valid_url_200_response (self):  # !
+        pathnames_to_test = (
+            'tracks_list',
+            'create_track',
+            'playlists_list',
+        )
+
+        for pathname in pathnames_to_test:
+            with self.subTest(pathname = pathname):  # !
+                response = self.guest_client.get(reverse(pathname))
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_invalid_url_404_response (self):
+        response = self.guest_client.get("/some_invalid_url_we_will_never_have_in_the_project")
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
